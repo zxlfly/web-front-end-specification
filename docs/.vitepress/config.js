9be56556c7d4958802764c9aa5acb83eecfe9935
sleep_fish <zxlfly@foxmail.com>
2023-04-20 11:18:19 +0800

    test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "前端开发规范", // 顶部左侧标题
-  base: "/specification/", // 项目的根路径
+  base: "/web-front-end-specification/", // 项目的根路径
   ignoreDeadLinks: true,// 构建时会忽略md中的外链
   markdown: {
     // theme: 'material-palenight',//md主题
